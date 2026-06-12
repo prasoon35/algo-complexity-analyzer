9a4630f2d5f15471dd1c7ddefcea1cc6a3672362
Prasooon <vermaprasoon9@gmail.com>
2026-06-12 18:30:11 +0530

    changed the user function

--- user_function.py
+++ user_function.py
@@ -6,11 +6,8 @@
 
 def mystery(arr):
     # example — replace with your own code
-    low, high = 0, len(arr) - 1
-    while low < high:
-        mid = low + (high - low) // 2
-        if arr[mid] < arr[mid + 1]:
-            low = mid + 1
-        else:
-            high = mid
-    return low
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] == arr[j]:
+                return True
+    return False
